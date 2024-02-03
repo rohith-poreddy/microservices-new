6c9621984bf670788362df6f64cd271d7152a180
rohith-poreddy <rohithreddyporeddy@gmail.com>
2024-02-02 22:33:12 -0500

    Communication betweeen order service and inventory service.

--- inventory-service/src/main/java/com/javadev/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/javadev/inventoryservice/service/InventoryService.java
@@ -1,9 +1,12 @@
 package com.javadev.inventoryservice.service;
 
+import com.javadev.inventoryservice.dto.InventoryResponse;
 import com.javadev.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +14,14 @@ public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
-    @Transactional
-    public boolean isInStock(String skuCode){
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    @Transactional(readOnly = true)
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                        InventoryResponse.builder()
+                                .skuCode(inventory.getSkuCode())
+                                .isInStock(inventory.getQuantity() > 0)
+                                .build()
+                ).toList();
     }
 }
--- order-service/src/main/java/com/javadev/orderservice/dto/OrderLineItemsDto.java
+++ order-service/src/main/java/com/javadev/orderservice/dto/OrderLineItemsDto.java
@@ -10,6 +10,8 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderLineItemsDto {
+
+    private Long id;
     private String skuCode;
     private BigDecimal price;
     private Integer quantity;
